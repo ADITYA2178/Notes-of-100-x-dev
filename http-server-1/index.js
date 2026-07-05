6f5f11b0741c8e2d6bb97e46af171610abea0e3b
Adityaj2902 <adityajofficial909@gmail.com>
2026-07-05 19:54:52 +0530

    Add response handler and important notes in Notes2-2.html and index.js; update package.json to include nodemon for development.

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -3,10 +3,26 @@ const port = 3000;
 
 const app = express();
 
+
+app.get("/request-handler", function(req, res) {
+    //HEADER , BODY , QUERY PARAMETER
+    //DO MACHINE LEARNING MODEL
+
+    res.json({
+        name:"Aditya Jaiswal",
+        age:20,
+        city:"New York"
+    })
+})
+
 app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+    res.send(<h1>Hello World</h1>);
+    // Backend can return html also 
+})
 
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
-});
+})
+
+//Homework:
+//1. Create a todo app that allows you to add, delete, and view todos.
